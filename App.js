19ebcb83f2618372995af7432a9dec7a958c9bcb
Jayasurya0327 <jayasurya.t@stickon.tech>
2024-07-08 14:59:59 +0530

    Error Handling

--- App.js
+++ App.js
@@ -1,74 +1,73 @@
 const express = require('express');
 const app = express();
-const port=3000;
+const port = 3000;
 const {
-    createUser,
-    readUsers,
-    updateUser,
-    deleteUser
-  } = require('./Service');
-  app.use(express.json());
-  //fetch all users
-  app.get('/api/users', async (req, res) => {
-    try{
+  createUser,
+  readUsers,
+  updateUser,
+  deleteUser
+} = require('./Service');
+app.use(express.json());
+//fetch all users
+app.get('/api/users', async (req, res) => {
+  try {
     res.send(await readUsers())
-    }
-    catch(err){
-      res.status(400).json({
-        message: err
-      });
-    }
-  });
-  //create new user
-  app.post('/api/newuser', async (req, res) => {
-    try{
+  }
+  catch (err) {
+    res.status(400).json({
+      message: err
+    });
+  }
+});
+//create new user
+app.post('/api/newuser', async (req, res) => {
+  try {
     const newUser = req.body;
     await createUser(newUser);
     res.status(201).json({
       message: 'User added successfully'
     });
   }
-  catch(err){
+  catch (err) {
     res.status(400).json({
       message: err
     });
   }
-    
-  });
-  //Update User
-  app.put('/api/updateuser', async (req, res) => {
-    try{
-      const email = req.query.email;
-      const updatedUser = req.body;
-      await updateUser(email,req.body);
-      res.status(201).json({
-          message:"User Updated successfully"
-      });
-    }
-    catch(err){
-      res.status(400).json({
-        message: err
-      });
-    }
-  });
-  //delete user
-  app.delete('/api/removeuser',async (req,res) => {
-    try{
-      const email = req.query.email;
-      await deleteUser(email);
-      res.status(201).json({
-          message:"User removed successfully"
-      });
-    }
-    catch(err){
-      res.status(400).json({
-        message: err
-      });
-    }
-  })
-  // Start the server
-  app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-  });
 
- 
+});
+//Update User
+app.put('/api/updateuser', async (req, res) => {
+  try {
+    const email = req.query.email;
+    const updatedUser = req.body;
+    await updateUser(email, req.body);
+    res.status(201).json({
+      message: "User Updated successfully"
+    });
+  }
+  catch (err) {
+    res.status(400).json({
+      message: err
+    });
+  }
+});
+//delete user
+app.delete('/api/removeuser', async (req, res) => {
+  try {
+    const email = req.query.email;
+    await deleteUser(email);
+    res.status(201).json({
+      message: "User removed successfully"
+    });
+  }
+  catch (err) {
+    res.status(400).json({
+      message: err
+    });
+  }
+})
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
--- Service.js
+++ Service.js
@@ -16,58 +16,57 @@ const User = mongoose.model('User', userSchema);
 
 // Function to connect to the database
 async function connectToDatabase() {
-    await mongoose.connect('mongodb://localhost:27017/Sample');
+  await mongoose.connect('mongodb://localhost:27017/Sample');
 }
 
 // Function to close the database connection
 async function closeDatabaseConnection() {
-    await mongoose.connection.close();
+  await mongoose.connection.close();
 }
 
 // Function to create a new user
 async function createUser(newuser) {
-     // Connect to the database
-     await connectToDatabase();
-    const newUser = new User(newuser);
-    await newUser.save();
-    await closeDatabaseConnection();
+  // Connect to the database
+  await connectToDatabase();
+  const newUser = new User(newuser);
+  await newUser.save();
+  await closeDatabaseConnection();
 }
 
 // Function to read all users
 async function readUsers() {
-    // Connect to the database
-    await connectToDatabase();
-    const users = await User.find({});
-    return users;
-    // Close the database connection
-    await closeDatabaseConnection();
+  // Connect to the database
+  await connectToDatabase();
+  const users = await User.find({});
+  // Close the database connection
+  await closeDatabaseConnection();
+  return users;
 }
 
 // Function to update a user by email
 async function updateUser(email, updates) {
-    // Connect to the database
-    await connectToDatabase();
-    const updatedUser = await User.findOneAndUpdate(
-      { email },
-      updates,
-      { new: true }
-    );
-    if(!updatedUser){
-      throw "User Not Found";
-    }
-    // Close the database connection
-    await closeDatabaseConnection();
+  // Connect to the database
+  await connectToDatabase();
+  const updatedUser = await User.findOneAndUpdate(
+    { email },
+    updates,
+    { new: true }
+  );
+  if (!updatedUser) {
+    throw "User Not Found";
+  }
+  // Close the database connection
+  await closeDatabaseConnection();
 }
 
 // Function to delete a user by email
 async function deleteUser(email) {
-    // Connect to the database
-    await connectToDatabase();
-    const deletedUser = await User.findOneAndDelete({ email });
-    if(!deletedUser){
-      throw "User Not Found!";
-    }
-    return deletedUser;
-    // Close the database connection
-    await closeDatabaseConnection();
+  // Connect to the database
+  await connectToDatabase();
+  const deletedUser = await User.findOneAndDelete({ email });
+  if (!deletedUser) {
+    throw "User Not Found!";
+  }
+  // Close the database connection
+  await closeDatabaseConnection();
 }
